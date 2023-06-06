ed0383777c38ad4ead70235a7da6cc5a1202e2ee
Merve Cetin <mervecetinnitec@gmail.com>
2023-06-06 16:00:53 +0300

    update security config

--- src/main/java/com/kodlamaio/commonpackage/utils/security/KeycloakJwtRoleConverter.java
+++ src/main/java/com/kodlamaio/commonpackage/utils/security/KeycloakJwtRoleConverter.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class KeycloakJwtRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+    private final static String ROLE_PREFİX="ROLE_";
     @Override
     public Collection<GrantedAuthority> convert(Jwt source) {
         return extractRoles(source);
@@ -20,6 +21,6 @@ public class KeycloakJwtRoleConverter implements Converter<Jwt, Collection<Grant
         var clains=jwt.getClaims();
         var realmAccess=(Map<String,Object>) clains.getOrDefault("realm_access", Collections.emptyMap());
         var roles =(List<String>) realmAccess.getOrDefault("roles",Collections.emptyList());
-        return roles.stream().map(role-> new SimpleGrantedAuthority(("ROLE_")+role)).collect(Collectors.toList());
+        return roles.stream().map(role-> new SimpleGrantedAuthority(ROLE_PREFİX+role)).collect(Collectors.toList());
     }
 }
